b29067ab3b2511f0e88c128f255759bda19f9b8a
Roger Madjos <roger.madjos@gmail.com>
2018-10-02 08:53:46 +0800

    build(*): build new version

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,9 +1,7 @@
 export default class AppError<IAttributes = Object> extends Error {
-    private _meta;
-    private _code;
-    constructor(code: string, message: string, meta?: IAttributes);
-    readonly code: string;
-    readonly meta: {};
+    code: string;
+    meta?: IAttributes | undefined;
+    constructor(code: string, message: string, meta?: IAttributes | undefined);
     toJSON(): IAttributes & {
         code: string;
         message: string;
--- build/index.js
+++ build/index.js
@@ -3,18 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class AppError extends Error {
     constructor(code, message, meta) {
         super(message);
-        this._code = code;
-        this._meta = meta;
+        this.code = code;
+        this.meta = meta;
         this.name = 'AppError';
     }
-    get code() {
-        return this._code;
-    }
-    get meta() {
-        return this._meta || {};
-    }
     toJSON() {
-        return Object.assign({}, (this._meta || {}), { code: this.code, message: this.message });
+        return Object.assign({}, (this.meta || {}), { code: this.code, message: this.message });
     }
 }
 exports.default = AppError;

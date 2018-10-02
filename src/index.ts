02a264177441341aece7179e22c60f40678c4510
Roger Madjos <roger.madjos@gmail.com>
2018-10-02 08:53:22 +0800

    feat(*): remove getters

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
 export default class AppError<IAttributes = Object> extends Error {
-  private _meta: IAttributes | undefined;
-  private _code: string;
-  constructor (code: string, message: string, meta?: IAttributes) {
+  constructor (public code: string, message: string, public meta?: IAttributes) {
     super(message);
-
-    this._code = code;
-    this._meta = meta;
     this.name = 'AppError';
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  get meta() {
-    return this._meta || {};
   }
 
   toJSON(): IAttributes & { code: string, message: string } {
     return {
-      ...(this._meta || {}) as any,
+      ...(this.meta || {}) as any,
       code: this.code,
       message: this.message,
     };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,16 +3,26 @@ import { expect } from 'chai';
 import AppError from '../src/index';
 
 describe('AppError', () => {
-  it('should create error', () => {
-    const error = new AppError('TEST_ERROR', 'Test error');
-    expect(error.toJSON()).to.deep.equal({ code: 'TEST_ERROR', message: 'Test error' });
-    expect(error.name).to.equal('AppError');
+  describe('Given no meta data', () => {
+    it('should create error', () => {
+      const error = new AppError('TEST_ERROR', 'Test error');
+
+      expect(error.meta).to.be.undefined;
+      expect(error.toJSON()).to.deep.equal({ code: 'TEST_ERROR', message: 'Test error' });
+      expect(error.name).to.equal('AppError');
+      expect(error.code).to.equal('TEST_ERROR');
+      expect(error.message).to.equal('Test error');
+    });
   });
 
-  it('should create error', () => {
-    const error = new AppError('TEST_ERROR', 'Test error', { one: 1 });
-    expect(error.meta).to.deep.equal({ one: 1 });
-    expect(error.toJSON()).to.deep.equal({ code: 'TEST_ERROR', message: 'Test error', one: 1 });
-    expect(error.name).to.equal('AppError');
+  describe('Given a meta data', () => {
+    it('should create error', () => {
+      const error = new AppError('TEST_ERROR', 'Test error', { one: 1 });
+      expect(error.meta).to.deep.equal({ one: 1 });
+      expect(error.toJSON()).to.deep.equal({ code: 'TEST_ERROR', message: 'Test error', one: 1 });
+      expect(error.name).to.equal('AppError');
+      expect(error.code).to.equal('TEST_ERROR');
+      expect(error.message).to.equal('Test error');
+    });
   });
 });
